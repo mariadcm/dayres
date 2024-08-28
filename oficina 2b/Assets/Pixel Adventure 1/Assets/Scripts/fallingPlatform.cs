05b29712ff59c3c51139859ab2888b67942a22d5
mariadcm <145375497+mariadcm@users.noreply.github.com>
2024-08-28 10:22:11 -0300

    modificaçoes 3

---
+++ oficina 2b/Assets/Pixel Adventure 1/Assets/Scripts/fallingPlatform.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallingPlatform : MonoBehaviour
+{
+    public float fallingTime;
+
+    private TargetJoint2D target;
+    private BoxCollider2D boxColl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GetComponent<TargetJoint2D>();
+        boxColl =  GetComponent<BoxCollider2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Invoke("falling", fallingTime);
+    }
+  
+   
+    }   
+          
+          void OnTriggerEnter2D(Collider2D collider)
+          {
+              if(collider.gameObject.layer == 9)
+          {
+            Destroy(gameObject);
+          }
+    } 
+
+void falling()
+{
+    target.enabled = false;
+    boxColl.isTrigger = true;
+  }
+}
